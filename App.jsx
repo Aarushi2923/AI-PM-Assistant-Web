e841629f64ab944d55bbb819eddad4247e8a0f42
Aarushi <aarushi.011004@gmail.com>
2025-08-28 15:58:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+function App() {
+  return (
+    <>
+      <h1>AI PM Assistant</h1>
+      <p>Your static site converted to React!</p>
+    </>
+  );
+}
+
+export default App;
